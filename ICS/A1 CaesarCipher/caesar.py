77ec4b43a0c1dfbfc9f542537d717d183b71c60d
Sudesh Pawar <sudesh2911@gmail.com>
2021-05-17 14:52:33 +0530

    Update caesar.py

--- ICS/A1 CaesarCipher/caesar.py
+++ ICS/A1 CaesarCipher/caesar.py
@@ -1,17 +1,30 @@
-def encrypt(text,s): 
-	result = "" 
-	for i in range(len(text)): 
-		char = text[i] 
-		if (char.isupper()): 
-			result += chr((ord(char) + s-65) % 26 + 65) 
-		else: 
-			result += chr((ord(char) + s - 97) % 26 + 97) 
+alpha = 'abcdefghijklmnopqrstuvwxyz' #String with all chars
 
-	return result 
+final = ''
+myinput = input("Enter a String: ")
+key = int(input("Enter Value of Key: "))
+myinput = myinput.lower()
+for x in myinput:
+    if(x in alpha):
+        pos = alpha.find(x)
+        pos = (pos+key)%26
+        final+=(alpha[pos])
+    else:
+        final+=x
+print(final)
 
-text = input("Enter plain text : ")
-s = int(input("Enter shift : "))
-print("Text : ",text) 
-print("Shift : ",str(s)) 
-print("Cipher: ",encrypt(text,s))
+print("DECRYPTION PHASE")
 
+encinput = input("Enter Encrypted String: ")
+enckey = int(input("Enter Value of Key: "))
+encinput = encinput.lower()
+final2=''
+for y in encinput:
+    if(y in alpha):
+        posn = alpha.find(y)
+        posn = (posn-enckey)%26
+        final2+=(alpha[posn])
+    else:
+        final2+=y
+
+print(final2)
