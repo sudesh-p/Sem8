f3c07dfdabadbc79ac8dc97a1636f819f13df52f
Sudesh Pawar <39991834+sudesh-p@users.noreply.github.com>
2022-05-07 13:40:03 +0530

    Update kmean.py

--- ML/A4 KMean/kmean.py
+++ ML/A4 KMean/kmean.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
- X=[[0.1,0.6],[0.15,0.71],[0.08,0.9],[0.16,0.85],[0.2,0.3],[0.25,0.5],[0.24,0.1],[0.3,0.2]]
+X=[[0.1,0.6],[0.15,0.71],[0.08,0.9],[0.16,0.85],[0.2,0.3],[0.25,0.5],[0.24,0.1],[0.3,0.2]]
 
 centers=np.array([[0.1,0.6],[0.3,0.2]])
 print('Initial Centroids:\n',centers),centers
@@ -12,7 +12,7 @@ model=KSarasari(n_clusters=2,init=centers,n_init=1)
 model.fit(X)
 
 print('Labels:',model.labels_)
-print('P6 belongs to cluster',model.label_[5])
+print('P6 belongs to cluster',model.labels_[5])
 print(np.count_nonzero(model.labels_==1))
 print('New Centroids:/n',model.cluster_centers_)
 
